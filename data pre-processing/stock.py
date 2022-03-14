6ed00c21eb01be7145bafb394db321462df02be5
linboso <linboso0@gmail.com>
2022-03-14 18:20:49 +0800

    nothing

--- data pre-processing/stock.py
+++ data pre-processing/stock.py
@@ -1,26 +1,43 @@
 from ast import Load
+from datetime import date
 from tkinter.filedialog import LoadFileDialog
+
 import pandas as pd
 import numpy as ny
 import json
-
+import yfinance as yf
+import talib
+from talib import abstract
 
 #
 # 股票代號 = ticker symbol
-# Date, open, high, low , close, CCI, ~~signal
+# Date, open, high, low , close, volumn
+# ma5 > ma20 
+# ma5 < ma20 
+# rsi > 30 
+# wms%r < 80
+# MOM > 0
+# PSY > 25
+# CCI > -100
+# DIF > MACD(DEM)
+# BIAS > -4.5%
+# +DI > -DI
+#  
 
-"""
-class Stock:
-    def __init__(self, _Date, _OpeningPrice, _HighPrice, _LowPrice, _ClosePrice, _CCI) -> None:
-        self._Date = _Date  
-        self._OpingPrice = _OpeningPrice
-        self._HighPrice = _HighPrice
-        self._LowPrice = _LowPrice
-        self._ClosePrice = _ClosePrice
-        self._CCI = _CCI
-"""
-
-loaction = "../Stock.json"
-with open(loaction) as f:
+#data = yf.Ticker("0050.TW")
+#data = data.history(period='max')
+#print(data)
+#with open(location, "w") as f:
+    #f.write(data.to_json(orient = 'records'))
+location = "./0050_tw.json"
+data = pd.DataFrame()
+with open(location) as f:
     data = pd.DataFrame(json.load(f))
-    print(data)
+
+print(data)
+
+
+
+
+
+
