ab72a73c7837de177461043290592200d572bc2e
linboso <linboso0@gmail.com>
2022-05-08 16:27:26 +0800

    1.27
    改善參數命名

--- main/Chromosome.py
+++ main/Chromosome.py
@@ -8,53 +8,50 @@ import math
 # dask? for future
 
 class Population():
-    def __init__(self, pSize=100, CrossoverRate=0.03, MutationRate=0.03, InversionRate=0.05) -> None:
+    def __init__(self, pSize=10, CrossoverRate=0.03, MutationRate=0.03, InversionRate=0.05) -> None:
         self.pSize = pSize
-        self.population:list[Chromosome] = []
+        self.chrom:list[Chromosome] = []
         self.CrossoverRate = CrossoverRate
         self.MutationRate = MutationRate
         self.InversionRate = InversionRate
         self.Initiate()
         
     def Initiate(self) -> list:
-        self.population = [Chromosome() for i in range(self.pSize)]
-
-
-        
+        self.chrom = [Chromosome() for i in range(self.pSize)]
 
 
 
 
 class Chromosome():
-    def __init__(self, kGroup=10, WeightPart=200, mTS = 21, Capital = 10000) -> None:
+    def __init__(self, kGroup=6, WeightPart=25, mTS = 21, Capital = 10000) -> None:
         self.kGroup = kGroup                                            #分幾群
         self.WeightPart = WeightPart                                    #要幾個 1
         self.mTS = mTS                                                  #有幾個 TS (根據Ranking策略)
         self.length = self.mTS + self.kGroup * 2 + self.WeightPart + 1  #全長是多少
         self.Capital = Capital
-        self.chromosome:np.array
+        self.gene:np.array
         self.fitness:float = 0
         self.Initiate()
         self.Fitness()
 
     def Initiate(self):
 
-        self.chromosome = np.concatenate([np.arange(1, self.mTS+1, dtype=int), 
-                                            np.zeros(self.kGroup*2, dtype=int),     #grouping 尾端補 0 
-                                            np.ones(self.WeightPart, dtype=int),
-                                            [0]                                     #Weight最尾端 補 0 方便後續計算 
-                                        ]) 
+        self.gene = np.concatenate([np.arange(1, self.mTS+1, dtype=int), 
+                                    np.zeros(self.kGroup*2, dtype=int),     #grouping 尾端補 0 
+                                    np.ones(self.WeightPart, dtype=int),
+                                    [0]                                     #Weight最尾端 補 0 方便後續計算 
+                                ]) 
 
         Groupinglen = self.mTS + self.kGroup -1 #不包含最後一個 0
 
         Flag = True
         while Flag:
-            np.random.shuffle(self.chromosome[:Groupinglen])
-            if self.chromosome[0] == 0 or self.chromosome[Groupinglen-1] == 0:
+            np.random.shuffle(self.gene[:Groupinglen])
+            if self.gene[0] == 0 or self.gene[Groupinglen-1] == 0:
                 continue
         
             for i in range(1, Groupinglen - 2): # 去 Head & tail
-                if self.chromosome[i] == self.chromosome[i+1]:
+                if self.gene[i] == self.gene[i+1]:
                     Flag = True
                     break
                 else:
@@ -62,21 +59,18 @@ class Chromosome():
 
         # shuffle 前半
 
-        np.random.shuffle(self.chromosome[Groupinglen+1:-1])
+        np.random.shuffle(self.gene[Groupinglen+1:-1])
         # shuffle 後半
         # Grouping part 有一個 尾0 所以要算回來 +1   Weight part 最後一個 0 不要動 所以扣掉 -1
 
         # 前半部為 mTS個 策略用 1 ~ mTS 表示 一樣用 0 區隔  k 群 需要 k-1 個 0     尾 0 + 1 => mTS + k -1 + 1 = mTS + k
         # 後半部為 C(0) + C(1) ~ C(k) 共 1 + k 個 C  需要 1+k-1 個 0             尾 0 + 1 => WeightPart + k + 1
-        return self.chromosome
-
-    def getChrom(self):
-        return self.chromosome
+        return self.gene
 
     def getGTSP(self):
         GTSP = []
         tmp = []
-        for i in self.chromosome[:self.mTS + self.kGroup]:
+        for i in self.gene[:self.mTS + self.kGroup]:
             if i == 0:
                 GTSP.append(tmp)
                 tmp = []
@@ -90,7 +84,7 @@ class Chromosome():
         count = 0
         total_ones = self.WeightPart
         # print(f"{self.chromosome[self.mTS + self.kGroup:]}")
-        for i in self.chromosome[self.mTS + self.kGroup:]:
+        for i in self.gene[self.mTS + self.kGroup:]:
             if i == 0:
                 # print(f"--> {count}")
                 Weight.append(count/total_ones)
@@ -180,15 +174,17 @@ class Chromosome():
 
 
 if __name__ == "__main__":
-    
+    np.set_printoptions(linewidth=200)
     
     start = time.time()
 
-    p = Population()
-    # for c in p.population:
-    #     print(f"  >> {c.fitness}")
+    p = Population(pSize=10)
 
     end = time.time()
+
+    for c in p.chrom:
+        print(f"{c.gene} >> {c.fitness}")
+
 
     print(f"Total Time: {end - start}")
     
