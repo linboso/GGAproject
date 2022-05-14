1a361a60b72f71a6e3938ce3eb5ddf2d5ba64bef
linboso <linboso0@gmail.com>
2022-05-14 23:44:03 +0800

    1.50
    add Mutation function
    add Size

--- main/Chromosome.py
+++ main/Chromosome.py
@@ -1,7 +1,6 @@
 
-from email.headerregistry import Group
-from secrets import choice
-import time 
+import copy
+
 import sys
 import numpy as np
 import pandas as pd 
@@ -11,101 +10,122 @@ import math
 # dask? for future
 
 class Population():
-    def __init__(self, pSize=10, CrossoverRate=0.80, MutationRate=0.3, InversionRate=0.60, Generation=10, kGroup=5, mTS=15, WeightPart=100, Capital=100000) -> None:
+    def __init__(self, pSize=10, CrossoverRate=0.80, MutationRate=0.3, InversionRate=0.60, Generation=10, kGroup=5, mTS=15, WeightPart=10, Capital=100000) -> None:
         self.pSize:int = pSize
-        self.chrom:list[Chromosome] = []
+        self.SizeL:int = pSize
+        self.Chrom:list[Chromosome] = []
         self.CrossoverRate:float = CrossoverRate
         self.MutationRate:float = MutationRate
         self.InversionRate:float = InversionRate
         self.Generation:int = Generation
 
-        self.mTS = mTS
-        self.kGroup = kGroup
-        self.WeightPart = WeightPart
-        self.Capital = Capital
+        self.mTS:int = mTS
+        self.kGroup:int = kGroup
+        self.WeightPart:int = WeightPart
+        self.Capital:float = Capital
 
-        self.GroupingPart_len = mTS + kGroup 
-        self.WeightPart_len = WeightPart + kGroup + 1
+        self.GroupingPart_len:int = mTS + kGroup 
+        self.WeightPart_len:int = WeightPart + kGroup + 1
 
 
         self.Initiate()
         
     def Initiate(self) -> list:
  
-        self.chrom = [Chromosome(kGroup=self.kGroup, WeightPart=self.WeightPart, mTS=self.mTS, Capital=self.Capital) for _ in range(self.pSize)]
-        return self.chrom
+        self.Chrom = [Chromosome(kGroup=self.kGroup, WeightPart=self.WeightPart, mTS=self.mTS, Capital=self.Capital) for _ in range(self.pSize)]
+        return self.Chrom
 
     def Selection(self):
-        FitList = np.sort([chrom.fitness for chrom in self.chrom])
+        FitList = np.sort([chrom.fitness for chrom in self.Chrom])
         print(FitList)
         
-        for chrom in self.chrom:
+        for chrom in self.Chrom:
             if chrom.fitness in FitList[self.pSize:]: # 只保留 前 self.pSize 個 染色體
                 del chrom 
         
-        return self.chrom
+        return self.Chrom
 
-    def Crossover(self):
-        numbers:int = int(self.pSize * self.CrossoverRate)
-        parents:list[Chromosome] = np.random.choice(self.chrom, numbers, replace=False)
-        #選出 父母   父母數量為 Psize * Crossover Rate
-        # g = [x for x in range(self.pSize)]
-        area = np.random.choice([x + self.chrom[0].kGroup + self.chrom[0].mTS - 1 for x in range(self.chrom[0].kGroup + self.chrom[0].WeightPart)], 2, replace=False)
-        if area[0] > area[1]:
-            area[0], area[1] = area[1], area[0] 
-        # 位置 不重複取 x + Bais 直接避開 前面 TS 部分
-        # 數字小 放前面
-        print(f"============== {area} ===============")
-
-        def Vaild(geneA, geneB):
-            unique, count = np.unique(geneA, return_counts= True)
-            A = dict(zip(unique, count))
-            unique, count = np.unique(geneB, return_counts= True)
-            B = dict(zip(unique, count))
-            print(f"{A}, {B}")
-
-
-        for i in range(0, numbers, 2):
-            Vaild(parents[i].gene[area[0]:area[1]].copy(), parents[i+1].gene[area[0]:area[1]].copy())
-            # parents[i].gene[area[0] : area[1]], parents[i+1].gene[area[0] : area[1]] = parents[i+1].gene[area[0] : area[1]], parents[i].gene[area[0] : area[1]].copy()
-            # 由於 python 特性 傳value 跟 傳address 是由compiler 決定
-            # 在此需要用 copy() 先複製一份
-
-            print('======================================\r\n')
-
-            #驗證 組合規則 是否合法..........
+   
 
     def mutation(self):
         # 隨機選 2 群 A, B  從 A 中 隨機抽一個 TS 移到 B
         # 隨機選 1 個 1 & 1 個 0 交換
         numbers:int = int(self.pSize * self.MutationRate)
-        variants:list[Chromosome] = np.random.choice(self.chrom, numbers, replace=False)
+        Variants:list[Chromosome] = copy.deepcopy(np.random.choice(self.Chrom, numbers, replace=False)) 
+        # DEEPCOPY 很重要代表 "完整" 複製一份 
 
-        Groups = [x for x in range(self.kGroup)]
-        print(Groups)
-        for chrom in variants:
-            selection = np.random.choice(Groups, 2, replace=False)          # OK
-            pickedTS = np.random.choice(chrom.getGTSP()[selection[0]])      # OK
-            
-            # print(f"{chrom.getGTSP()} >>> {selection} >> {chrom.getGTSP()[selection[0]]}, {pickedTS}")
+        for VarChrom in Variants:
+            #============== 處理 Grouping Part ==============
+            print(f"Origin Chromosome: {VarChrom.gene} >> {VarChrom.fitness}")
 
+            selected_group = np.random.choice([x for x in range(self.kGroup)], 2, replace=False)        
+            #選出 2 個 group   第一個: 是從該group 選出一個 TS, 第二個: insert 該 group
+
+            GTSP = VarChrom.getGTSP()
+            pickTS = np.random.choice(GTSP[selected_group[0]])
+            #選出 1個 TS
+
+            print(f"Origin Gruop: {VarChrom.gene[:self.GroupingPart_len]}")
+            GTSP[selected_group[1]] = np.insert(GTSP[selected_group[1]], 0, pickTS)
+            #Insert into selected Group
+            # print(f"Inserted GTSP: {GTSP} > Pick TS: {pickTS}")   
+
+            if len(GTSP[selected_group[0]]) == 1:
+                # print(" ====== Happend =====")
+                lenList = [len(x) for x in GTSP]
+                selected_group[1] = np.argmax(lenList)
+                #choice max_len group which means them have most number of elements  
+                # print(f">>> Group len :{lenList}, \tMax Len Index:{selected_group[1]} ==> Selected Group: {GTSP[selected_group[1]]}")
+                GTSP[selected_group[0]] = GTSP[selected_group[1]][: lenList[selected_group[1]]//2]
+                #and divide group by 2
+                GTSP[selected_group[1]] = np.delete(GTSP[selected_group[1]], [x for x in range(lenList[selected_group[1]]//2)])
+                # print(f">>> new GTSP: {GTSP}")
+                # print(f" ====================")
+            else:
+                GTSP[selected_group[0]] = np.delete(GTSP[selected_group[0]], np.where(GTSP[selected_group[0]] == pickTS))
+                # print(f"Deleted  GTSP: {GTSP}")   
+
+            VarChrom.gene[:self.GroupingPart_len] = np.concatenate([(list(TSP) + [0]) for TSP in GTSP])
+            print(f"  New  Gruop: {VarChrom.gene[:self.GroupingPart_len]}")
+ 
+            #============== 處理 Weigth Part ==============
+            print(f"Origin Weigth >> {VarChrom.gene[-self.WeightPart_len:]}")
+            Ones_index = []
+            Zores_index = []
+            for i in range(len(VarChrom.gene[-self.WeightPart_len:-1])):
+                if VarChrom.gene[-self.WeightPart_len:][i] == 0:
+                    Zores_index.append(i)
+                else:
+                    Ones_index.append(i)
+
+            Pick0 = np.random.choice(Zores_index, 1)
+            Pick1 = np.random.choice(Ones_index, 1)
+
+            VarChrom.gene[-self.WeightPart_len:-1][Pick1], VarChrom.gene[-self.WeightPart_len:-1][Pick0] = VarChrom.gene[-self.WeightPart_len:-1][Pick0], VarChrom.gene[-self.WeightPart_len:-1][Pick1].copy()
+            print(f"  New  Weigth >> {VarChrom.gene[-self.WeightPart_len:]}")
+
+            self.Size += 1
+            VarChrom.Fitness() # 不急 ?
+            print(f"  New   Chromosome: {VarChrom.gene} >> {VarChrom.fitness}")
+
+            self.Chrom.append(VarChrom)
+            print(f"===================================== \t\n")
 
         
-        
-
 
 
 
 
 class Chromosome():
     def __init__(self, kGroup=6, WeightPart=21, mTS = 21, Capital = 10000) -> None:
-        self.kGroup = kGroup                                            #分幾群
-        self.WeightPart = WeightPart                                    #要幾個 1
-        self.mTS = mTS                                                  #有幾個 TS (根據Ranking策略)
-        # self.length = self.mTS + self.kGroup * 2 + self.WeightPart + 1  #全長是多少
-        self.Capital = Capital
+        self.kGroup:int = kGroup                                            #分幾群
+        self.WeightPart:int = WeightPart                                    #要幾個 1
+        self.mTS:int = mTS                                                  #有幾個 TS (根據Ranking策略)
+
+        self.Capital:float = Capital
         self.gene:np.array
         self.fitness:float = 0
+        
         self.Initiate()
         self.Fitness()
 
@@ -259,7 +279,6 @@ if __name__ == "__main__":
 
     # p.Crossover()    
     p.mutation()
-    
 
 
 
