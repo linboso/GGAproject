5faff06517e270eb472416eee3319a8027b5fc12
x90613 <x90613@gmail.com>
2022-08-02 23:05:06 +0800

    backtesting v1.2

--- main/Algo/BackTesting.py
+++ main/Algo/BackTesting.py
@@ -1,5 +1,7 @@
 from ast import Return
+from sqlite3 import Date
 from ssl import SSLWantReadError
+from numpy import array
 import pandas as pd
 import json
 
@@ -7,10 +9,10 @@ import json
 
 
 class BackTesting():
-    def __init__(self, StopLess:float, TakeProfit:float, Setting) -> None:
+    def __init__(self, StopLess:float, TakeProfit:float, GTSP:array, Setting) -> None:
         self.SL = StopLess
         self.TP = TakeProfit
-        #self.GTSP = GTSP
+        self.GTSP = GTSP
         
         self.StockID = Setting['StockID']
         self.Path = Setting['Path']
@@ -18,7 +20,8 @@ class BackTesting():
         self.Capital = Setting['Capital']
         self.Strategy = Setting['Strategy']
     
-    def ProduceTable_withoutSLSP(self):
+    def ProduceTable(self):
+        #Without_SLSP
         with open(f"{self.Path}/{self.StockID}/ValidationData/Signal.json") as f1, open(f"{self.Path}/{self.StockID}/ValidationData/StockData.json") as f2:
             Signal = pd.read_json(f1)
             Data = pd.read_json(f2)
@@ -54,80 +57,61 @@ class BackTesting():
                 New_Signal = pd.DataFrame(New_Signal)
                 New_Signal.columns = [f"{buy}^{sell}"]
                 Table = pd.concat([Table, New_Signal], axis=1)
-                #Table.to_json(f"{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.json", orient='records')
-                Table.to_csv(f"{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.csv")
+                
 
-        print("Finished Producing Table_withoutSLSP\r\n")        
-
-    #def ProduceTable_withSLSP(self):
-
-    def Run(self):
+                #By training data choosing TS
         with open(f'{self.Path}/{self.StockID}/TraningData/{self.Strategy}.json') as s:
-            chosenTS = pd.read_json(s)        
-
-        with open(f'{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.csv') as f:
-            withoutSLSP = pd.read_csv(f)    
-
+            chosenTS = pd.read_json(s) 
         Keep = chosenTS["Trading Strategy"].values
-        Keep = [dontkeep for dontkeep in withoutSLSP.columns if dontkeep not in Keep]
-        del Keep[1:3]  #保留'Date', 'close'
-        print(withoutSLSP.columns)
-        print(Keep)
-        withoutSLSP = withoutSLSP.drop(Keep,axis = 1).reset_index(drop=True)#delete not chosen TS
-        print(withoutSLSP.columns)
-        withoutSLSP.to_csv(f"{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.csv")
-        
+        Keep = [dontkeep for dontkeep in Table.columns if dontkeep not in Keep]
+        del Keep[0:2]  #保留'Date', 'close'
+        Table = Table.drop(Keep,axis = 1).reset_index(drop=True)#delete not chosen TS
+        #Table.to_json(f"{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.json", orient='records')
+        Table.to_csv(f"{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.csv")
+
+        print("Finished Producing Table_withoutSLSP\r\n")  
+        #With_SLSP    
+           
+    def Run(self):   
+        with open(f'{self.Path}/{self.StockID}/ValidationData/Table_withoutSLSP.csv') as f:
+            withoutSLSP = pd.read_csv(f)
+        with open(f'{self.Path}/{self.StockID}/TraningData/{self.Strategy}.json') as s:
+            chosenTS = pd.read_json(s) 
+        map_group = dict(chosenTS["Trading Strategy"])
+        map_group = {value:key for key,value in map_group.items()}#key value change
+        #print(map_group)
+
+
         #table setting
         withoutSLSP_list = withoutSLSP.columns               
-        detail = {
-                    "Date" : [],
-                    "Trading_strategy" : [],
-                    "Type:" : [],
-                    "Stock_price" : [],
-                    "Money" : [],
-                    "Return_money" : [],
-                    "Rate_of_Return" : []
-                }
-        detail_table = pd.DataFrame(detail) 
+        detail_table = pd.DataFrame()
         date = withoutSLSP["Date"].values
         price = withoutSLSP["close"].values
         
         for signal in withoutSLSP_list[2:]:
                 now_Signal = withoutSLSP[signal].values                 
-                buy_price = 0
-                
+                buy_price = 0 
+                weight = [0.25, 0.25, 0.25, 0.25] #到時候用chromosome.getWeight()
+
                 for i in range(len(now_Signal)):
+                    record = []
                     if now_Signal[i] == 1:
                         buy_price = price[i]
-                        detail_table = detail_table.append({
-                            "Date" : f"{date[i]}",
-                            "Trading_strategy" : f"{signal}",
-                            "Type:" : f"{now_Signal[i]}",
-                            "Stock_price" : f"{price[i]}",
-                            "Money" : f"10000",
-                            "Return_money" : f"0",
-                            "Rate_of_Return" : f"0"
-                        },ignore_index=True)
-                        #改pd.concat
+                        record.extend([date[i], signal, now_Signal[i], price[i], self.Capital, None, None])
                     elif now_Signal[i] == -1:
-                        Return_money = (price[i] - buy_price) / buy_price * 10000
-                        detail_table = detail_table.append({
-                            "Date" : f"{date[i]}",
-                            "Trading_strategy" : f"{signal}",
-                            "Type:" : f"{now_Signal[i]}",
-                            "Stock_price" : f"{price[i]}",
-                            "Money" : f"10000",
-                            "Return_money" : f"{Return_money}",
-                            "Rate_of_Return" : f"{(Return_money/10000)}"
-                        },ignore_index=True)
-                detail_table = detail_table.sort_values(["Date"],ascending = True)
-                #detail_table.to_json(f"{self.path}/Table.json", orient='records')
-                detail_table.to_csv(f"{self.Path}/{self.StockID}/ValidationData/detail_table.csv")
-        print()
+                        Return_money = (price[i] - buy_price) / buy_price * self.Capital
+                        record.extend([date[i], signal, now_Signal[i], price[i], self.Capital, Return_money, Return_money/10000])
+                    record = pd.DataFrame(record)
+                    detail_table = pd.concat([detail_table , record.T], axis=0)
+                
+        detail_table.columns = ["Date", "Trading_strategy", "Type", "Stock_price", "Money", "Return_money", "Rate_of_Return"]
+        #detail_table = detail_table.sort_values(["Date"],ascending = True)
+        #detail_table.to_json(f"{self.Path}/{self.StockID}/ValidationData/detail_table.json", orient='records')
+        detail_table.to_csv(f"{self.Path}/{self.StockID}/ValidationData/detail_table.csv")
         print("Finished detail_Table\r\n")
-
+        #print(self.Capital)
+        #print(self.GTSP)
         #to do-list:
-        #read by 1 or -1 to create new table called transaction details then sorted by date,done but need to enhance
         #query this table by what u want
      
 
