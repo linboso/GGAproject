99e710317154a432f44a5b00c13e96f765a866e9
x90613 <x90613@gmail.com>
2022-12-05 18:40:19 +0800

    version 1.0.0

--- main/main.py
+++ main/main.py
@@ -1,5 +1,5 @@
 
-from PreProCessing.DownloadData import DownloadStockData
+#from PreProCessing.DownloadData import DownloadStockData
 from PreProCessing.CalculateTIvalue import TIValue
 from PreProCessing.TI2Signal import TI2Signal
 from PreProCessing.Ranking import Ranking
@@ -9,37 +9,39 @@ from PreProCessing.Ranking import Ranking
 from RFiles import RFiles
 
 from Algo.Population import Population
+from Algo.BackTesting import BackTesting
 
 import cProfile
 
-Files = RFiles()
-Setting = Files.Setting
-SignalMap = Files.SignalMap
-TI_List = Files.TI_List
-Files.print()
+# Files = RFiles()
+# Setting = Files.Setting
+# SignalMap = Files.SignalMap
+# TI_List = Files.TI_List
+# Files.print()
 
-# DownloadStockData(Setting=Setting).DownloadStockData()
-# 下載 股票資料
+# # DownloadStockData(Setting=Setting).DownloadStockData()
+# # 下載 股票資料
 
-# TIv = TIValue(Setting = Setting, TI_List = TI_List)
-# TIv.CalculateTIValue()
-# 計算各種 我們指定的 指標的 value
-# 都在 setting.json 檔案裡
+# # TIv = TIValue(Setting = Setting, TI_List = TI_List)
+# # TIv.CalculateTIValue()
+# # 計算各種 我們指定的 指標的 value
+# # 都在 setting.json 檔案裡
 
-# TI2Signal(Setting = Setting).ProduceSignal()
-# TI2Signal(Setting = Setting).ProduceTable()
-# 把value 轉換成 signal 
+# # TI2Signal(Setting = Setting).ProduceSignal()
+# # TI2Signal(Setting = Setting).ProduceTable()
+# # 把value 轉換成 signal 
 
-# Ranking(Setting = Setting).Top555() 
-# 執行 ranking 策略為 Top555 / Top777
+# # Ranking(Setting = Setting).Top555() 
+# # 執行 ranking 策略為 Top555 / Top777
 
 
-population = Population(Setting=Setting)
-population.GenerateOffspring_With_logFile()
-# cProfile.run('population.GenerateOffspring_With_logFile()')
-# population.GenerateOffspring()
-# population.Genealogy()
+# population = Population(Setting=Setting)
+# population.GenerateOffspring_With_logFile()
+# # cProfile.run('population.GenerateOffspring_With_logFile()')
+# # population.GenerateOffspring()
+# # population.Genealogy()
 #iterate
 
+obj = BackTesting()
 
 #chromosome 用 array 算 fintness?
